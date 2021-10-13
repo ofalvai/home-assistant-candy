571a395b3ff5f1e961d029ef0bf4c01fb17cd0de
terminet85 <terminet@gmail.com>
2021-10-13 16:15:39 +0200

    add test file

---
+++ tests/test_sensor_tumble_dryer.py
@@ -0,0 +1,43 @@
+"""Tests for various sensors"""
+from pytest_homeassistant_custom_component.common import MockConfigEntry, load_fixture
+from pytest_homeassistant_custom_component.test_util.aiohttp import AiohttpClientMocker
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry, device_registry
+
+from .common import init_integration
+
+
+async def test_main_sensor_idle(hass: HomeAssistant, aioclient_mock: AiohttpClientMocker):
+    await init_integration(hass, aioclient_mock, load_fixture("tumble_dryer/idle.json"))
+
+    state = hass.states.get("sensor.tumble_dryer")
+
+    assert state
+    assert state.state == "Idle"
+    assert state.attributes == {
+        'program': 1,
+        'remaining_minutes': 150,
+        'remote_control': True,
+        'dry_level': 2,
+        'dry_level_now': 1,
+        'refresh': False,
+        'need_clean_filter': False,
+        'watertank_full': False,
+        'door_close': True,
+        'friendly_name': 'Tumble dryer',
+        'icon': 'mdi:tumble-dryer'
+    }
+
+
+async def test_main_sensor_device_info(hass: HomeAssistant, aioclient_mock: AiohttpClientMocker):
+    await init_integration(hass, aioclient_mock, load_fixture("tumble_dryer/idle.json"))
+
+    er = entity_registry.async_get(hass)
+    dr = device_registry.async_get(hass)
+    entry = er.async_get("sensor.tumble_dryer")
+    device = dr.async_get(entry.device_id)
+
+    assert device
+    assert device.manufacturer == "Candy"
+    assert device.name == "Tumble dryer"
+    assert device.suggested_area == "Bathroom"
